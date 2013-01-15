1701f0531e97d8fb111191b6f8c8ed56413ed59c
Nicomplex <nico.pellissier.94@gmail.com>
2013-01-15 23:50:10 +0100

    Changements

---
+++ TRODS - Copie/TRODS/TRODS/sources/outils/AbstractMap.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TRODS
+{
+    class AbstractMap : AbstractScene
+    {
+        public struct Element
+        {
+            public Element(AnimatedSprite _s, float _speed = 0, float _verticalSpeed = 0, bool _repeating = false, bool _foreground = false)
+            {
+                sprite = _s;
+                speed = _speed;
+                verticalSpeed = _verticalSpeed;
+                repeating = _repeating;
+                foreground = _foreground;
+            }
+            public AnimatedSprite sprite;
+            public float speed;
+            public float verticalSpeed;
+            public bool repeating;
+            public bool foreground;
+            public void WindowResized(Rectangle oldWinSize, Rectangle winSize)
+            {
+                sprite.windowResized(winSize);
+                speed *= winSize.Width / oldWinSize.Width;
+                verticalSpeed *= winSize.Height / oldWinSize.Height;
+            }
+        }
+
+        protected List<Element> _elements;
+        public List<Element> Elements
+        {
+            get { return _elements; }
+            set { _elements = value; }
+        }
+        protected Rectangle _windowSize;
+        public Rectangle WindowSize
+        {
+            get { return _windowSize; }
+            set { _windowSize = value; }
+        }
+        protected Vector2 _vuePosition;
+        public Vector2 VuePosition
+        {
+            get { return _vuePosition; }
+            set { _vuePosition = value; }
+        }
+        protected List<Rectangle> _visitable;
+        public List<Rectangle> Visitable
+        {
+            get { return _visitable; }
+            set { _visitable = value; }
+        }
+
+        public AbstractMap(Rectangle windowSize)
+        {
+            _windowSize = windowSize;
+            _elements = new List<Element>();
+            _visitable = new List<Rectangle>();
+        }
+        public AbstractMap(Rectangle windowSize, List<Element> elements, Vector2 vuePosition, List<Rectangle> visitableArea)
+        {
+            _windowSize = windowSize;
+            _elements = elements;
+            _vuePosition = vuePosition;
+            _visitable = visitableArea;
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            foreach (Element s in _elements)
+                s.sprite.LoadContent(content);
+        }
+        public override void Update(float elapsedTime)
+        {
+            foreach (Element s in _elements)
+                s.sprite.Update(elapsedTime);
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle p;
+            foreach (Element e in _elements)
+            {
+                if (e.repeating)
+                {
+
+                    p = e.sprite.Position;
+
+                    if (p.X > _windowSize.Width)
+                        p.X = p.X - p.Width;
+                    else if (p.X < -p.Width)
+                        p.X = p.X + p.Width;
+
+                    e.sprite.Position = p;
+
+                    e.sprite.Draw(spriteBatch);
+                    if (p.X > 0)
+                        e.sprite.Draw(spriteBatch, new Vector2(p.X - p.Width, p.Y));
+                    if (p.X - p.Width < _windowSize.Width)
+                        e.sprite.Draw(spriteBatch, new Vector2(p.X + p.Width, p.Y));
+                }
+                else
+                    e.sprite.Draw(spriteBatch);
+            }
+        }
+        public virtual bool Moving(Vector2 destination, bool performMove = true)
+        {
+            Point p = new Point((int)(destination.X + VuePosition.X), (int)(destination.Y + VuePosition.Y));
+            foreach (Rectangle r in Visitable)
+                if (r.Contains(p))
+                {
+                    if (performMove)
+                    {
+                        foreach (Element s in _elements)
+                            s.sprite.Position = new Rectangle(
+                                s.sprite.Position.X - (int)(destination.X * s.speed),
+                                s.sprite.Position.Y - (int)(destination.Y * s.verticalSpeed),
+                                s.sprite.Position.Width,
+                                s.sprite.Position.Height);
+                        _vuePosition.X += destination.X;
+                        _vuePosition.Y += destination.Y;
+
+                        for (int i = _visitable.Count - 1; i >= 0; i--)
+                        {
+                            if (_elements.ElementAt(i).foreground)
+                            {
+                                i = -1;
+                                float s = _elements.ElementAt(i).speed;
+                                float vs = _elements.ElementAt(i).verticalSpeed;
+                                List<Rectangle> n = new List<Rectangle>();
+                                Rectangle rip;
+                                foreach (Rectangle v in _visitable)
+                                {
+                                    rip = v;
+                                    rip.X += (int)((float)destination.X * s);
+                                    rip.Y += (int)((float)destination.Y * vs);
+                                    n.Add(rip);
+                                }
+                                _visitable = n;
+                            }
+                        }
+                    }
+                    return true;
+                }
+            return false;
+        }
+        public override void WindowResized(Rectangle rect)
+        {
+            foreach (Element e in _elements)
+                e.WindowResized(_windowSize, rect);
+
+            float x = (float)rect.Width / (float)_windowSize.Width;
+            float y = (float)rect.Height / (float)_windowSize.Height;
+            List<Rectangle> n = new List<Rectangle>();
+            Rectangle r;
+            foreach (Rectangle rectal in _visitable)
+            {
+                r = rectal;
+                r.X = (int)((float)r.X * x);
+                r.Y = (int)((float)r.Y * y);
+                r.Width = (int)((float)r.Width * x);
+                r.Height = (int)((float)r.Height * y);
+                n.Add(r);
+            }
+            _visitable = n;
+
+            _vuePosition.X = _vuePosition.X * x;
+            _vuePosition.Y = _vuePosition.Y * y;
+
+            _windowSize = rect;
+        }
+    }
+}
---
+++ TRODS - Copie/TRODS/TRODS/sources/outils/Sprite.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TRODS
+{
+    class Sprite
+    {
+        public string AssetName { get; set; }
+        public Texture2D Texture { get; set; }
+        private Rectangle _position;
+        public Rectangle Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        public Vector2 Direction { get; set; }
+        private float _vitesse;
+        public float Vitesse
+        {
+            get
+            {
+                return _vitesse;
+            }
+            set
+            {
+                _vitesse = value;
+                _relativeSpeed = _vitesse / _position.Width;
+            }
+        }
+
+        private bool _isRelativePos;
+        private float _relativePosX;
+        private float _relativePosY;
+        private float _relativeWidth;
+        private float _relativeHeight;
+        private float _relativeSpeed;
+
+        /// <summary>
+        /// Si les 2 derniers parametres sont donnes,
+        /// le Sprite d'adapte automatiquement aux
+        /// redimensionnements de la fenetre par
+        /// l'appel de windowResized()
+        /// </summary>
+        /// <param name="a_position">Dimenson du Sprite dans la fenetre</param>
+        /// <param name="windowWidth">Largeur de la fenetre</param>
+        /// <param name="windowHeight">Hauteur de la fenetre</param>
+        public Sprite(Rectangle a_position, Rectangle windowSize = new Rectangle(), string assetName = "")
+        {
+            _position = a_position;
+            Direction = new Vector2();
+            _vitesse = 0;
+            AssetName = assetName;
+            if (windowSize.Width > 0 && windowSize.Height > 0)
+            {
+                _relativePosX = (float)a_position.X / (float)windowSize.Width;
+                _relativePosY = (float)a_position.Y / (float)windowSize.Height;
+                _relativeWidth = (float)a_position.Width / (float)windowSize.Width;
+                _relativeHeight = (float)a_position.Height / (float)windowSize.Height;
+                _isRelativePos = true;
+                _relativeSpeed = 0;
+            }
+            else
+                _isRelativePos = false;
+        }
+
+        /// <summary>
+        /// Constructeur de copie
+        /// </summary>
+        /// <param name="s">Sprite a copier</param>
+        public Sprite(Sprite s)
+        {
+            AssetName = s.AssetName;
+            _position = s.Position;
+            Direction = s.Direction;
+            _vitesse = s.Vitesse;
+            _isRelativePos = s._isRelativePos;
+            _relativePosX = s._relativePosY;
+            _relativePosY = s._relativePosY;
+            _relativeWidth = s._relativeWidth;
+            _relativeHeight = s._relativeHeight;
+            _relativeSpeed = s._relativeSpeed;
+        }
+
+        /// <summary>
+        /// Chargement de la texture
+        /// </summary>
+        /// <param name="content">Gertionnaire de contenu de XNA</param>
+        /// <param name="assetName">Nom de la texture</param>
+        public virtual void LoadContent(ContentManager content, string assetName = "")
+        {
+            if (assetName == "")
+                assetName = AssetName;
+            else
+                AssetName = assetName;
+            Texture = content.Load<Texture2D>(assetName);
+        }
+        /// <summary>
+        /// Assigne la texture
+        /// </summary>
+        /// <param name="tex">Texture</param>
+        public virtual void LoadContent(Texture2D tex)
+        {
+            Texture = tex;
+        }
+
+        /// <summary>
+        /// Met a jour la _position du sprite
+        /// </summary>
+        /// <param name="elapsedTime">Temps ecoule depuis le dernier appel de la fonction</param>
+        public virtual void Update(float elapsedTime)
+        {
+            _position = new Rectangle((int)(_vitesse * Direction.X * elapsedTime) + _position.X,
+                                      (int)(_vitesse * Direction.Y * elapsedTime) + _position.Y,
+                                      _position.Width, _position.Height);
+        }
+
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, _position, Color.White);
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, Color color)
+        {
+            spriteBatch.Draw(Texture, _position, color);
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, byte alpha)
+        {
+            spriteBatch.Draw(Texture, _position, Color.FromNonPremultiplied(255, 255, 255, alpha));
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, Color color, int X, int Y)
+        {
+            spriteBatch.Draw(Texture, new Rectangle(X, Y, _position.Width, _position.Height), color);
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Draw(Texture, new Rectangle((int)position.X, (int)position.Y, _position.Width, _position.Height), Color.White);
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, Rectangle destination, Rectangle source, Color color)
+        {
+            spriteBatch.Draw(Texture, destination, source, color);
+        }
+
+        /// <summary>
+        /// Active l'adaptation automatique de la texture
+        /// au redomensionnement de la fenetre
+        /// </summary>
+        /// <param name="a_position">_position actuelle du Sprite</param>
+        /// <param name="windowWidth">Largeur de la fenetre</param>
+        /// <param name="windowHeight">Hauteur de la fenetre</param>
+        public virtual void setRelatvePos(Rectangle a_position, int windowWidth, int windowHeight)
+        {
+            _position = a_position;
+            if (windowWidth > 0 && windowHeight > 0)
+            {
+                _relativePosX = (float)a_position.X / (float)windowWidth;
+                _relativePosY = (float)a_position.Y / (float)windowHeight;
+                _relativeWidth = (float)a_position.Width / (float)windowWidth;
+                _relativeHeight = (float)a_position.Height / (float)windowHeight;
+                _isRelativePos = true;
+            }
+            else
+                _isRelativePos = false;
+        }
+        /// <summary>
+        /// Adapte la taille et la _position de la texture
+        /// et la _vitesse a celle de la fenetre
+        /// </summary>
+        /// <param name="rect"></param>
+        public virtual void windowResized(Rectangle rect)
+        {
+            if (_isRelativePos)
+            {
+                _position = new Rectangle(
+                    (int)(_relativePosX * (float)rect.Width),
+                    (int)(_relativePosY * (float)rect.Height),
+                    (int)(_relativeWidth * (float)rect.Width),
+                    (int)(_relativeHeight * (float)rect.Height));
+                _vitesse = _relativeSpeed * (float)_position.Width;
+            }
+        }
+        /// <summary>
+        /// Libere les textures
+        /// </summary>
+        public virtual void Dispose()
+        {
+            Texture.Dispose();
+        }
+    }
+}
