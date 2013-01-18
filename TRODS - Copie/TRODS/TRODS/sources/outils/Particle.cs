e615ef544fec96acb69951e9c3f1910ef65e9c14
Nicomplex <nico.pellissier.94@gmail.com>
2013-01-18 02:19:32 +0100

    Moteur à particule (Beta)

---
+++ TRODS - Copie/TRODS/TRODS/sources/outils/Particle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TRODS
+{
+    class Particle
+    {
+        //propriétés des particules
+        Texture2D Texture { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Speed { get; set; }
+        public float Angle { get; set; }
+        public float AngularSpeed { get; set; }
+        public Color Color { get; set; }
+        public float Size { get; set; }
+        public int LifeTime { get; set; }
+
+        public Particle(Texture2D texture, Vector2 position, Vector2 speed, float angle, float angularSpeed,
+                        Color color, float size, int lifeTime)
+        {
+            Texture = texture;
+            Position = position;
+            Speed = speed;
+            Angle = angle;
+            AngularSpeed = angularSpeed;
+            Color = color;
+            Size = size;
+            LifeTime = lifeTime;
+        }
+
+
+        public void Update()
+        {
+            LifeTime--;
+            Position += Speed;
+            Angle += AngularSpeed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, null, Color, Angle,
+                new Vector2(Texture.Width / 2, Texture.Height / 2), Size, SpriteEffects.None,0f);
+        }
+    }
+}
---
+++ TRODS - Copie/TRODS/TRODS/sources/outils/ParticleEngine.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace TRODS
+{
+    class ParticleEngine
+    {
+        private Random random;
+        public Rectangle EmitterLocation { get; set; }
+        private List<Particle> particles;
+        private List<Texture2D> textures;
+
+        private Vector4 speedRange;
+        private Vector2 angleRange;
+        private Vector2 angularSpeedRange;
+        private Vector2 scaleRange;
+        private Vector2 lifeTimeRange;
+
+        public ParticleEngine(Rectangle emitterLocation, Vector4 speedRange, Vector2 angleRange,
+                                Vector2 angularSpeedRange, Vector2 scaleRange, Vector2 lifeTimeRange)
+        {
+            random = new Random();
+            EmitterLocation = emitterLocation;
+            particles = new List<Particle>();
+            textures = new List<Texture2D>();
+
+            this.speedRange = speedRange;
+            this.angleRange = angleRange;
+            this.angularSpeedRange = angularSpeedRange;
+            this.scaleRange = scaleRange;
+            this.lifeTimeRange = lifeTimeRange;
+        }
+
+        private Particle GenerateParticle()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 position = new Vector2(random.Next(EmitterLocation.X, EmitterLocation.X + EmitterLocation.Width + 1),
+                                         random.Next(EmitterLocation.Y, EmitterLocation.Y + EmitterLocation.Height + 1));
+            /*Vector2 speed = new Vector2(
+                    (float)(random.NextDouble() * random.Next((int)speedRange.X, (int)speedRange.Y+1)),
+                    (float)(random.NextDouble() * random.Next((int)speedRange.Z, (int)speedRange.W + 1)));*/
+            Vector2 speed = new Vector2(
+                            1f * (float)(random.NextDouble() * 2 - 1),
+                            1f * (float)(random.NextDouble() * 2 - 1));
+            float angle = (float)random.NextDouble() * (float)random.Next((int)angleRange.X, (int)angleRange.Y + 1);
+            float angularSpeed = 0.1f * (float)(random.NextDouble() * 1.25f - 1);
+            Color color = new Color(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256),
+                0);
+            float size = (float)random.NextDouble() * (float)(random.Next((int)scaleRange.X, (int)scaleRange.Y + 1));
+            int lifeTime = random.Next((int)lifeTimeRange.X, (int)lifeTimeRange.Y + 1);
+
+            return new Particle(texture, position, speed, angle, angularSpeed, color, size, lifeTime);
+        }
+
+        public void LoadContent(ContentManager content, List<string> assetNames)
+        {
+            foreach (string s in assetNames)
+                textures.Add(content.Load<Texture2D>(s));
+        }
+
+        public void Update(int nbNewParticle = 10)
+        {
+            for (int i = 0; i < nbNewParticle; i++)
+            {
+                particles.Add(GenerateParticle());
+            }
+
+            for (int i = 0; i < particles.Count; i++)
+            {
+                particles[i].Update();
+                if (particles[i].LifeTime <= 0)
+                {
+                    particles.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Particle p in particles)
+                p.Draw(spriteBatch);
+        }
+    }
+}
