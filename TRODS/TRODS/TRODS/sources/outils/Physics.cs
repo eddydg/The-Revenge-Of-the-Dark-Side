fd3e58c82c6dd1b75f469c22cd6e615a726f2a3b
Kuchera <link5525@hotmail.com>
2013-01-26 16:17:31 +0100

    Changements dans Physics

--- TRODS/TRODS/TRODS/sources/outils/Physics.cs
+++ TRODS/TRODS/TRODS/sources/outils/Physics.cs
@@ -7,26 +7,53 @@ namespace TRODS.sources.outils
 {
     class Physics
     {
+        public int MaxHeight
+        {
+            get;
+            set
+            {
+                if (value < 0)
+                    MaxHeight = 0;
+            }
+        }
+        public int TimeOnFlat
+        {
+            get;
+            set
+            {
+                if (value < 0)
+                    TimeOnFlat = 0;
+            }
+        }
 
         /// <summary>
-        /// Fonction qui calcule la trajectoire d'un saut.
+        /// Constructeur.
         /// </summary>
-        /// <param name="maxHeight">Hateur maximale.</param>
-        /// <param name="timeOnFlat">Duree du saut sur une surface plate. (en ms)</param>
-        /// <param name="time">Temps (0 au debut) (en ms)</param>
-        /// <returns>Hauteur en fonction du temps.</returns>
-        public static int Jump(int time, int maxHeight = 200, int timeOnFlat = 1000)
+        /// <param name="MaxHeight">Hauteur maximale du saut.</param>
+        /// <param name="TimeOnFlat">Duree du saut sur une surface plate. (en ms)</param>
+        public Physics(int MaxHeight = 200, int TimeOnFlat = 1000)
         {
+        }
+
+        /// <summary>
+        /// Calcul de la hauteur du saut.
+        /// </summary>
+        /// <param name="time">Temps. (en ms, partant de 0)</param>
+        /// <returns>Hauteut du saut.</returns>
+        public int Jump(int time)
+        {
+            // f(x) = -(x+timeOnflat)2 + maxHeight
             return 0;
         }
 
         /// <summary>
-        /// Fonction qu calcule la trajectoire d'une chute.
+        /// Calcul de la hauteur de la chute.
         /// </summary>
-        /// <param name="speed">Vitesse de la chute.</param>
-        /// <returns></returns>
-        public static int Fall(int speed = 1)
+        /// <param name="time">Temps. (en ms, partant de 0)</param>
+        /// <returns>Hauteur de la chute. (negative)</returns>
+        public int Fall(int time)
         {
+            // f(x) = -x2
             return 0;
         }
 
