031ec922fde03c2e861df63ca7fd9fb592ce7bf2
kuchera <link5525@hotmail.com>
2013-06-16 11:06:52 +0200

    Affichage partiel de TextSprite

--- TRODS/TRODS/TRODS/sources/graphics/TextSprite.cs
+++ TRODS/TRODS/TRODS/sources/graphics/TextSprite.cs
@@ -19,11 +19,14 @@ namespace TRODS
 
         public Color Color { get; set; }
 
+        private int _showedCharacters;
+
         public TextSprite(string assetName, Rectangle windowSize, Rectangle position, Color color, string text = "")
             : base(position, windowSize, assetName)
         {
             this.Color = color;
             this.Text = text;
+            _showedCharacters = Text.Length;
         }
 
         public override void LoadContent(ContentManager content)
@@ -39,27 +42,14 @@ namespace TRODS
         public override void Draw(SpriteBatch spriteBatch)
         {
             float num1 = this.min((float)this.Position.Width / this._spriteFont.MeasureString(this.Text).X, (float)this.Position.Height / this._spriteFont.MeasureString(this.Text).Y);
-            SpriteBatch spriteBatch1 = spriteBatch;
-            SpriteFont spriteFont = this._spriteFont;
-            string text = this.Text;
-            Rectangle position1 = this.Position;
-            double num2 = (double)position1.Location.X;
-            position1 = this.Position;
-            double num3 = (double)position1.Location.Y;
-            Vector2 position2 = new Vector2((float)num2, (float)num3);
-            Color color = this.Color;
-            double num4 = 0.0;
-            Vector2 zero = Vector2.Zero;
-            double num5 = (double)num1;
-            int num6 = 0;
-            double num7 = 0.0;
-            spriteBatch1.DrawString(spriteFont, text, position2, color, (float)num4, zero, (float)num5, (SpriteEffects)num6, (float)num7);
+            Vector2 position2 = new Vector2((float)Position.Location.X, (float)Position.Location.Y);
+            spriteBatch.DrawString(_spriteFont, Text.Substring(0, _showedCharacters), position2, Color, (float)0, Vector2.Zero, num1, (SpriteEffects)0, (float)0);
         }
 
         public override void Draw(SpriteBatch spriteBatch, Rectangle position)
         {
             float scale = this.min((float)position.Width / this._spriteFont.MeasureString(this.Text).X, (float)position.Height / this._spriteFont.MeasureString(this.Text).Y);
-            spriteBatch.DrawString(this._spriteFont, this.Text, new Vector2((float)position.Location.X, (float)position.Location.Y), this.Color, 0.0f, Vector2.Zero, scale, SpriteEffects.None, 0.0f);
+            spriteBatch.DrawString(this._spriteFont, this.Text.Substring(0, _showedCharacters), new Vector2((float)position.Location.X, (float)position.Location.Y), this.Color, 0.0f, Vector2.Zero, scale, SpriteEffects.None, 0.0f);
         }
 
         public override void Draw(SpriteBatch spriteBatch, Color color, int X, int Y)
@@ -74,7 +64,7 @@ namespace TRODS
             double num5 = (double)num1;
             int num6 = 0;
             double num7 = 0.0;
-            spriteBatch1.DrawString(spriteFont, text, position2, color, (float)num4, zero, (float)num5, (SpriteEffects)num6, (float)num7);
+            spriteBatch1.DrawString(spriteFont, text.Substring(0, _showedCharacters), position2, color, (float)num4, zero, (float)num5, (SpriteEffects)num6, (float)num7);
         }
 
         public override void Draw(SpriteBatch spriteBatch, Color color)
@@ -93,7 +83,7 @@ namespace TRODS
             double num5 = (double)num1;
             int num6 = 0;
             double num7 = 0.0;
-            spriteBatch1.DrawString(spriteFont, text, position2, color, (float)num4, zero, (float)num5, (SpriteEffects)num6, (float)num7);
+            spriteBatch1.DrawString(spriteFont, text.Substring(0, _showedCharacters), position2, color, (float)num4, zero, (float)num5, (SpriteEffects)num6, (float)num7);
         }
 
         private float min(float a, float b)
@@ -104,6 +94,16 @@ namespace TRODS
         public Vector2 GetOriginalTextSize()
         {
             return this._spriteFont.MeasureString(this.Text);
+        }
+
+        public void SetShowedCharacters(int count)
+        {
+            if (count < 0)
+                _showedCharacters = 0;
+            else if (count > Text.Length)
+                _showedCharacters = Text.Length;
+            else
+                _showedCharacters = count;
         }
     }
 }
